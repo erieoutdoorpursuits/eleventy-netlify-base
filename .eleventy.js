4344bf6239cfa7712267433d9800b0edc5767d82
Jim <eocbiker@gmail.com>
2025-01-08 19:16:49 -0500

    update eleventy base

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addWatchTarget("src/assets")
+
   return {
     dir: {
       input: "src",
--- src/posts/post_1.md
+++ src/posts/post_1.md
@@ -4,7 +4,7 @@ date: 2023-10-01T00:00:00.000Z
 decription: decription My Second First Post today 
 
 ---
-This is my first post
+This is my first post1
 
 {{ title }}
 
